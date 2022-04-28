dd6094523a8ba5cfd3f3415a4c58efd7bfe18a46
m.mortezaie <mrtz.mrtzi.1989@gmail.com>
2022-04-28 14:55:00 +0430

    add dot env and variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 // getting-started.js
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err+'dsfdfs'));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('db is connected')
     
 }
