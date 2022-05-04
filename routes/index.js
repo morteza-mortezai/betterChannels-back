316235a5cbb5a68e065e3a9cbd4e216a381d6e0b
m.mortezaie <mrtz.mrtzi.1989@gmail.com>
2022-05-04 10:14:23 +0430

    up

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const postControllers=require('../controllers/channel')
 const router=new Router();
 router.get('/',postControllers.get)
 router.post('/create',postControllers.create)
-router.get('/getById/:id',postControllers.getById)
+router.get('/:id',postControllers.getById)
 module.exports=router
