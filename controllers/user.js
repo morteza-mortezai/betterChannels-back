d2ba1340c0ad44c5b7cf98489a4161e8ffefe31f
m.mortezaie <mrtz.mrtzi.1989@gmail.com>
2022-05-04 23:52:46 +0430

    user reg addded

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,7 @@ exports.create = async (req, res) => {
         await User.create(req.body)
         return res.json({})
     } catch (err) {
-        res.status(403).json({ err })
+        res.status(400).json({ err })
     }
 
 }
