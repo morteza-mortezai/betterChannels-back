57530a9b6544a96109ca39e0ea4c9e166c5e0c05
m.mortezaie <mrtz.mrtzi.1989@gmail.com>
2022-05-04 18:00:26 +0430

    up

--- controllers/user.js
+++ controllers/user.js
@@ -5,17 +5,17 @@ exports.create = async (req, res) => {
     // 2. check if email exist
     // 3. create
     try {
-        const { email } = req.body
-        const user = await User.findOne({ email })
-        if (user) {
-           return res.json('این ایمیل وجود دارد')
-        }
-        await User.userValidation(req.body)
+        // const { email } = req.body
+        // const user = await User.findOne({ email })
+        // if (user) {
+        //    return res.json('این ایمیل وجود دارد')
+        // }
+        // await User.userValidation(req.body)
         const new_user = await User.create(req.body)
         res.json({data:'salam'})
         return res.json(new_user)
     } catch (err) {
-        res.json(err)
+        res.json({d:'asdasdasdas'})
     }
 
 }
