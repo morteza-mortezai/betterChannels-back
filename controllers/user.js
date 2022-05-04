373b56e679d32b8cf4462bb8333886b0f0df3b0d
m.mortezaie <mrtz.mrtzi.1989@gmail.com>
2022-05-05 00:20:59 +0430

    user reg addded

--- controllers/user.js
+++ controllers/user.js
@@ -3,15 +3,15 @@ const { User } = require('../models/user')
 exports.create = async (req, res) => {
     try {
         const { email } = req.body
-        const user = await User.findOne({ email })
-        if (user) {
-            return res.json({ err: 'این ایمیل وجود دارد' })
-        }
+        // const user = await User.findOne({ email })
+        // if (user) {
+        //     return res.status(400).json({ err: 'این ایمیل وجود دارد' })
+        // }
         await User.userValidation(req.body)
         await User.create(req.body)
         return res.json({})
     } catch (err) {
-        res.status(400).send({ err })
+        res.status(400).json({ err: 'email already exists' })
     }
 
 }
