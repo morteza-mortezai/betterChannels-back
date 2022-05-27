211a982bc1a89789e86b3ce29da31e2b350495dc
morteza-mortezai <mrtz.mrtzi.1989@gmail.com>
2022-05-27 19:48:20 +0430

    forget password

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/user')
 var bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const fetch = require('node-fetch')
 const { sendEmail } = require('../utils/mailer')
 
 exports.create = async (req, res, next) => {
@@ -23,7 +24,7 @@ exports.create = async (req, res, next) => {
         )
 
         const token = jwt.sign({ userId: createdUser._id }, process.env.JWT_SECRET)
-        const link = `http://localhost:5000/user/verify?token=${token}`
+        const link = `${process.env.DOMAIN_URL}verify?token=${token}`
 
         sendEmail(email, fullName, "تایید حساب کاربری  ", `
         با سلام
@@ -41,8 +42,32 @@ exports.create = async (req, res, next) => {
 }
 
 exports.handleLogin = async (req, res, next) => {
-    const { email, password } = req.body
+    const { email, password, token } = req.body
+    // recaptcha token
+    if (!token) {
+        const error = new Error({ message: 'recaptcha is not valid' })
+        throw error
+    }
+
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${token}&remoteip=${req.connection.remoteAddress}`
+
     try {
+        // verify recaptcha response
+        const response = await fetch(verifyUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+        });
+
+        const json = await response.json();
+
+        if (!json.success) {
+            const error = new Error({ message: 'recaptcha is not valid' })
+            throw error
+        }
+
         const user = await User.findOne({ email })
         // not found user
         if (!user) {
@@ -69,15 +94,125 @@ exports.handleLogin = async (req, res, next) => {
 
 exports.verify = async (req, res, next) => {
 
-    const { token } = req.query
+    const { token } = req.body
+
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+
     const { userId } = decodedToken
-    console.log('userID ', userId)
+
+
     try {
         const user = await User.findOne({ _id: userId })
         user.active = true
         await user.save()
         res.json({ message: 'ایمیل شما با موفقیت تایید شد' })
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+exports.forgetPass = async (req, res, next) => {
+    const { email, token } = req.body
+
+    // recaptcha token
+    if (!token) {
+        const error = new Error({ message: 'recaptcha is not valid' })
+        throw error
+    }
+
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${token}&remoteip=${req.connection.remoteAddress}`
+
+    try {
+        // verify recaptcha response
+        const response = await fetch(verifyUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+        });
+
+        const json = await response.json();
+
+        if (!json.success) {
+            const error = new Error({ message: 'recaptcha is not valid' })
+            throw error
+        }
+
+        const user = await User.findOne({ email })
+        // not found user
+        if (!user) {
+            const error = new Error('کاربر مورد نظر یافت نشد')
+            error.statusCode = 404;
+            throw error
+        }
+
+        const timeStamp = Date.now()
+
+        // produce token with timestamp
+        const token = jwt.sign({ userId: user._id, timeStamp }, process.env.JWT_SECRET)
+
+        const link = `${process.env.DOMAIN_URL}resetPass?token=${token}`
+
+        sendEmail(user.email, user.fullName, "بازیابی رمز عبور", `
+        با سلام
+        <br>
+        برای بازیابی رمز عبور روی لینک زیر کلیک کنید
+        <br>
+        ${link}
+        `)
+
+        return res.status(200).json({ message: "ایمیل بازیابی کلمه عبور با موفقیت ارسال شد" })
+
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+exports.resetPass = async (req, res, next) => {
+    const { token, password, resetToken } = req.body
+console.log('token for google',token)
+    try {
+
+        // 1. verify recaptcha response
+
+        const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${token}&remoteip=${req.connection.remoteAddress}`
+        console.log('verifyUrl', verifyUrl)
+        const response = await fetch(verifyUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            },
+        });
+        const json = await response.json();
+        if (!json.success) {
+            console.log('response2', json)
+            const error = new Error({ message: 'recaptcha is not valid' })
+            throw error
+        }
+
+        // 2. find user and change password
+        const nowTimeStamp = Date.now()
+        const decodedToken = jwt.verify(resetToken, process.env.JWT_SECRET)
+        const { userId, timeStamp } = decodedToken
+        console.log(' userId, timeStamp', userId, timeStamp)
+        if (nowTimeStamp - timeStamp > 15 * 60 * 6000) {
+            const error = new Error({ message: 'لینک ارسال شده متقضی شده لطفا دوباره امتحان کنید .' })
+            throw error
+        }
+        const user = await User.findOne({ _id: userId })
+        console.log('user', user)
+        user.password = password
+        await user.save()
+
+        // 3. make new token for login user
+        const token2 = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+
+        return res.status(200).json({ message: "کلمه عبور با موفقیت تغییر پیدا کرد .", user: { token: token2, fullName: user.fullName } })
+
     } catch (err) {
         next(err)
     }
